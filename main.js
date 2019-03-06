47d74083f4f66f7a861e1fbb462f49274b5ba66c
Nat <natalie.hoang91@gmail.com>
2019-03-06 16:39:29 +0700

    updated with background color

--- main.js
+++ main.js
@@ -3,27 +3,52 @@ function getRandomNumber() {
     return Math.floor(Math.random() * 100 + 1);
 }
 
-
-
 let randomNumber = getRandomNumber(100);
+
+console.log(randomNumber);
+let result = "";
+let guessBtn = document.getElementById("guessBtn");
+
+
+// console.log('Guess Button check');
+// console.log(guessBtn);
+
+// let myColor = "";
+
+function color(myColor) {
+    let a = document.getElementById("comment");
+    console.log(a)
+    a.classList.add(myColor);
+    console.log('After adding');
+    console.log(a);
+}
 
 function checkGuess() {
     let guessBox = document.getElementById("guessBox");
     let userNumber = guessBox.value;
-    let guessBtn = document.getElementById("guessBtn");
+    let comment = document.getElementById("comment");
+
 
     if (isNaN(userNumber) || userNumber == "") {
         alert("Can only enter a number");
-    } else if (userNumber < randomNumber) {
-        console.log("Your number is lower");
+    } else
+    if (userNumber < randomNumber) {
+        result = ("Your number is lower");
+        color("alertMessLow");
     } else
     if (userNumber > randomNumber) {
-        console.log("Your number is higher");
+        result = ("Your number is higher");
+        color("alertMessHigh");
     } else
     if (userNumber == randomNumber) {
-        console.log("BINGO");
+        result = ("BINGO");
+        color("alertMessOk");
     }
+    comment.innerHTML = result;
 }
-
-
 guessBtn.addEventListener("click", checkGuess);
+
+// let testButton = document.getElementById("testBtn");
+
+
+// testButton.addEventListener("click", color);
