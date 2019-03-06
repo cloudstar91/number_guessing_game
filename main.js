8908817e19b85f8bcf3c34067419f6a1da308ea5
Nat <natalie.hoang91@gmail.com>
2019-03-06 23:03:18 +0700

    updated, not yet fixed turnCOunt bug

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let turnCount = 10;
+let turnRemainSpan = document.getElementById("turnRemain");
+turnRemainSpan.innerHTML = "Remaining attemp: " + turnCount;
+
+
 function getRandomNumber() {
 
     return Math.floor(Math.random() * 100 + 1);
@@ -8,47 +13,76 @@ let randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 let result = "";
 let guessBtn = document.getElementById("guessBtn");
+// let resetBtn = document.getElementById("resetBtn");
 
 
-// console.log('Guess Button check');
-// console.log(guessBtn);
 
-// let myColor = "";
 
 function color(myColor) {
+
     let a = document.getElementById("comment");
-    console.log(a)
-    a.classList.add(myColor);
-    console.log('After adding');
-    console.log(a);
+
+
+    a.className = (myColor);
+
 }
 
 function checkGuess() {
+
+
+
     let guessBox = document.getElementById("guessBox");
     let userNumber = guessBox.value;
     let comment = document.getElementById("comment");
-
 
     if (isNaN(userNumber) || userNumber == "") {
         alert("Can only enter a number");
     } else
     if (userNumber < randomNumber) {
         result = ("Your number is lower");
+        turnCount--;
         color("alertMessLow");
+
     } else
     if (userNumber > randomNumber) {
         result = ("Your number is higher");
+        turnCount--;
         color("alertMessHigh");
+
     } else
     if (userNumber == randomNumber) {
         result = ("BINGO");
+
         color("alertMessOk");
+
     }
-    comment.innerHTML = result;
+    if (turnCount === 0) {
+        turnRemainSpan.innerHTML = "Over. Please click New Game!";
+        comment.innerHTML = "";
+        color("alertMess");
+        return;
+    } else if (turnCount > 0) {
+        comment.innerHTML = result;
+        turnRemainSpan.innerHTML = "Remaining attemp: " + turnCount;
+    }
+
+    console.log(turnCount);
 }
+
+// function Reset() {
+//     document.getElementById("myForm").reset();
+// }
+
 guessBtn.addEventListener("click", checkGuess);
+// resetBtn.addEventListener("click", Reset);
 
 // let testButton = document.getElementById("testBtn");
 
 
 // testButton.addEventListener("click", color);
+
+// function countDownTurn(x) {
+//     let i = 10;
+//     for (i = 10, i <= 10, i--) {
+
+//     }
